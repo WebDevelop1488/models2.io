24f8fed7451c0d955c248e6085177e4d78ddea9f
WebDevelop1488 <128897109+WebDevelop1488@users.noreply.github.com>
2023-06-03 16:26:22 +0300

    2

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-// ... код обработки маршрутов ...
-
-app.listen(3000, () => {
-  console.log('Сервер запущен на порту 3000');
-});
